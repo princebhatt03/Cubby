c4a33d1a2f98b557fb3661f8ee833581a9fb9e75
Prince Bhatt <prince25io044@satiengg.in>
2024-02-15 20:03:49 +0530

    Website Done

---
+++ script.js
@@ -0,0 +1,9 @@
+function nextPage() {
+  window.location.href = 'page2.html';
+}
+function nextPage2() {
+  window.location.href = 'page3.html';
+}
+function nextPage3() {
+  window.location.href = 'page4.html';
+}
